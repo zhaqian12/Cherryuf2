d76e4d3a47d26e41af088e0860a2217bf8495fd1
zhaqian <80608733+zhaqian12@users.noreply.github.com>
2023-05-01 17:45:45 +0800

    update header

--- ports/ArteryTek/at32f413/board_flash.c
+++ ports/ArteryTek/at32f413/board_flash.c
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2018 Ha Thach for Adafruit Industries
+ * Copyright (c) 2023 Zhaqian
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
